3a71b68dc2ccbaae455230db9edce9c55490146b
Evan Purcer <ep@e2.gg>
2024-11-07 09:55:50 +0000

    add context functions to create and update rooms

--- lib/cc/chat.ex
+++ lib/cc/chat.ex
@@ -19,4 +19,16 @@ defmodule CC.Chat do
   def get_first_room!() do
     Repo.one!(from r in Room, limit: 1, order_by: [asc: :name])
   end
+
+  def create_room(attrs) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_room(%Room{} = room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
+  end
 end
